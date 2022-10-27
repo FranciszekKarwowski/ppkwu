b1ee116a94af8e259680256a6034cdbe26a0cf35
FranciszekKarwowski <franek.karwowski.9@gmail.com>
2022-10-27 13:30:05 +0200

    add rev service

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,29 +3,39 @@ import http.server
 import socketserver
 import os
 import datetime
+from urllib.parse import urlparse, parse_qs
 
 #print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
-    def do_GET(self):
+	def do_GET(self):
 
-        print(self.path)
+		print(self.path)
         
-        if self.path == '/':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-        elif self.path == '/time':
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()
-            self.wfile.write(datetime.datetime.now().strftime("%H:%M:%S").encode("UTF-8"))
-        else:
-            super().do_GET()
+		if self.path == '/':
+			self.protocol_version = 'HTTP/1.1'
+			self.send_response(200)
+			self.send_header("Content-type", "text/html; charset=UTF-8")
+			self.end_headers()            
+			self.wfile.write(b"Hello World!\n")
+		elif self.path == '/time':
+			self.protocol_version = 'HTTP/1.1'
+			self.send_response(200)
+			self.send_header("Content-type", "text/html; charset=UTF-8")
+			self.end_headers()
+			self.wfile.write(datetime.datetime.now().strftime("%H:%M:%S").encode("UTF-8"))
+		elif '/rev' in self.path:
+			self.protocol_version = 'HTTP/1.1'
+			self.send_response(200)
+			self.send_header("Content-type", "text/plain; charset=UTF-8")
+			self.end_headers()
+			query_components = parse_qs(urlparse(self.path).query)
+			str = query_components["str"] 
+			rev_str = str[0][::-1]
+			self.wfile.write(rev_str.encode('UTF-8'))
+		else:
+			super().do_GET()
     
 # --- main ---
 
